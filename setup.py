ac52d9b62f6e0bc9bc1cb8f42ba58807705e3108
mikepan <mikepan@2039adbc-8964-11de-ae39-2786408ab335>
2010-01-29 09:18:20 +0000

    exclude test dir

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pypatterns',
     version='0.1',
-    packages=find_packages('src'),
+    packages=find_packages(
+        'src',exclude=["*.test", "*.test.*", "test.*", "test"]),
     package_dir={'':'src'}
 )
