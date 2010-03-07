4202c0c719fa3986d33970541686d775050a3dce
mikepan <mikepan@2039adbc-8964-11de-ae39-2786408ab335>
2010-03-07 17:56:43 +0000

    updating version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypatterns',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(
         'src',exclude=["*.test", "*.test.*", "test.*", "test"]),
     package_dir={'':'src'},
