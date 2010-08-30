387d864af2b97ca401519a3b0b6e657b6a595111
mikepan <mikepan@2039adbc-8964-11de-ae39-2786408ab335>
2010-08-30 11:17:33 +0000

    update version tag to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypatterns',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(
         'src',exclude=["*.test", "*.test.*", "test.*", "test"]),
     package_dir={'':'src'},
